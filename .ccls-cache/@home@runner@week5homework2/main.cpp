ac05565eb4481a899dd23e2b16b3f17c93e92b6f
Yisi Liu <85250232+yisliu@users.noreply.github.com>
2022-07-08 17:44:00 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@week5homework2/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+  //variable for input
+  int a;
+  int b;
+  cin>>a>>b;
+  //time a and b for square
+  int c = a*b;
+  //use premade function
+  c = sqrt(c);
+  //output
+  cout<<c;
+}
